104e2261425926747fee0766f0f625fff13ea28f
conrev <andrewmahisa@gmail.com>
2022-10-09 16:19:04 +1100

    empty scene

---
+++ include/ray.h
@@ -0,0 +1,27 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include "vec3.h"
+
+class ray
+{
+public:
+    ray() = default;
+    ray(const point3 &origin, const vec3 &direction) : m_origin(origin), m_direction(direction)
+    {
+    }
+
+    point3 eval(double t)
+    {
+        return m_origin + m_direction * t;
+    }
+
+    point3 origin() const { return m_origin; }
+    vec3 direction() const { return m_direction; }
+
+private:
+    point3 m_origin;
+    vec3 m_direction;
+};
+
+#endif
--- include/vec3.h
+++ include/vec3.h
@@ -7,38 +7,38 @@
 class vec3
 {
 public:
-    vec3() : data{0.0f, 0.0f, 0.0f} {}
-    vec3(double x, double y, double z) : data{x, y, z} {}
+    vec3() : m_data{0.0f, 0.0f, 0.0f} {}
+    vec3(double x, double y, double z) : m_data{x, y, z} {}
 
     double x() const
     {
-        return data[0];
+        return m_data[0];
     }
     double y() const
     {
-        return data[1];
+        return m_data[1];
     }
     double z() const
     {
-        return data[2];
+        return m_data[2];
     }
 
     // TODO : huh??
     vec3 operator-() const
     {
-        return vec3(-data[0], -data[1], -data[2]);
+        return vec3(-m_data[0], -m_data[1], -m_data[2]);
     }
 
     double &operator[](int i)
     {
-        return data[i];
+        return m_data[i];
     }
 
     vec3 &operator+=(const vec3 &rhs)
     {
-        this->data[0] = this->x() + rhs.x();
-        this->data[1] = this->y() + rhs.y();
-        this->data[2] = this->z() + rhs.z();
+        this->m_data[0] = this->x() + rhs.x();
+        this->m_data[1] = this->y() + rhs.y();
+        this->m_data[2] = this->z() + rhs.z();
         return *this;
     }
 
@@ -49,9 +49,9 @@ public:
 
     vec3 &operator*=(const double &rhs)
     {
-        this->data[0] = this->x() * rhs;
-        this->data[1] = this->y() * rhs;
-        this->data[2] = this->z() * rhs;
+        this->m_data[0] = this->x() * rhs;
+        this->m_data[1] = this->y() * rhs;
+        this->m_data[2] = this->z() * rhs;
         return *this;
     }
 
@@ -67,16 +67,72 @@ public:
 
     double length_squared() const
     {
-        return data[0] * data[0] + data[1] * data[1] + data[2] * data[2];
+        return m_data[0] * m_data[0] + m_data[1] * m_data[1] + m_data[2] * m_data[2];
+    }
+
+    vec3 &normalize()
+    {
+        return (*this) /= (*this).length();
     }
 
 private:
-    double data[3];
+    double m_data[3];
 };
 
 inline std::ostream &operator<<(std::ostream &out, const vec3 &v)
 {
     return out << v.x() << ' ' << v.y() << ' ' << v.z();
 }
+
+inline vec3 operator+(const vec3 &op1, const vec3 &op2)
+{
+    return vec3(op1.x() + op2.x(), op1.y() + op2.y(), op1.z() + op2.z());
+}
+
+inline vec3 operator-(const vec3 &op1, const vec3 &op2)
+{
+    return vec3(op1.x() - op2.x(), op1.y() - op2.y(), op1.z() - op2.z());
+}
+
+inline vec3 operator*(const vec3 &op1, const vec3 &op2)
+{
+    return vec3(op1.x() * op2.x(), op1.y() * op2.y(), op1.z() * op2.z());
+}
+
+inline vec3 operator*(double scale, const vec3 &op2)
+{
+    return vec3(scale * op2.x(), scale * op2.y(), scale * op2.z());
+}
+
+inline vec3 operator*(const vec3 &op1, double scale)
+{
+    return scale * op1;
+}
+
+inline vec3 operator/(vec3 v, double t)
+{
+    return (1 / t) * v;
+}
+
+inline double dot(const vec3 &v1, const vec3 &v2)
+{
+    return v1.x() * v2.x() + v1.y() * v2.y() + v1.z() * v2.z();
+}
+
+inline vec3 cross(const vec3 &u, const vec3 &v)
+{
+    return vec3(u.y() * v.z() - u.z() * v.y(),
+                u.z() * v.x() - u.x() * v.z(),
+                u.x() * v.y() - u.y() * v.x());
+}
+
+inline vec3 unit_vector(const vec3 &v)
+{
+    return v / v.length();
+}
+
+// Type aliases for vec3
+using point3 = vec3; // 3D point
+using color = vec3;  // RGB color
 
 #endif
